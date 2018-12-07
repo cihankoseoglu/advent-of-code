51960c2d934094e610efe87425167d1c60743b46
Cihan Koseoglu <cihan@cihankoseoglu.com>
2018-12-07 21:36:41 +0300

    Add part two

--- problem1/main.js
+++ problem1/main.js
@@ -1,9 +1,37 @@
 const fs = require('fs')
 
+// Read the input file
 const data = fs.readFileSync('./input.txt', 'utf8')
+
+
+// Part 1
 
 const result = data.split('\n')
     .map(x => parseInt(x, 10))
     .reduce((a = 0, b) => a + b)
 
 console.log(result)
+
+// Part 2
+
+let map = {};
+let repeat;
+
+let parsedArray = data.split('\n')
+  .map(x => parseInt(x));
+
+let previousResult = 0;
+
+while(repeat == null) {
+  previousResult = parsedArray.reduce((a, b) => {
+    let sum = a + b;
+    if (repeat == null && map[sum]) {
+        repeat = sum;
+    }
+
+    map[sum] = map[sum] ? ++map[sum] : 1;
+    return sum;
+  }, previousResult);
+}
+
+console.log(repeat)
